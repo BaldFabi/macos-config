c76d9dd2a8332c3628f2175b89e5169d700b2556
BaldFabi <thebaldfabi@gmail.com>
2024-10-26 01:30:06 +0200

    refactor: starship styling

--- settings.sh
+++ settings.sh
@@ -6,4 +6,4 @@ defaults write com.apple.dock largesize -int 90
 defaults write com.apple.dock magnification -bool true
 defaults write com.apple.menuextra.battery ShowPercent -bool true
 defaults write com.apple.driver.AppleMultitouchTrackpad Clicking -bool true
-
+defaults write com.apple.dock autohide-delay -int 0
