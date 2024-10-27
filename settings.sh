aa7a682e5507f82fa0f50d445137650eb68e4766
BaldFabi <thebaldfabi@gmail.com>
2024-10-27 14:30:29 +0100

    feat: install prettier globally via settings.sh

--- settings.sh
+++ settings.sh
@@ -7,3 +7,10 @@ defaults write com.apple.dock magnification -bool true
 defaults write com.apple.menuextra.battery ShowPercent -bool true
 defaults write com.apple.driver.AppleMultitouchTrackpad Clicking -bool true
 defaults write com.apple.dock autohide-delay -int 0
+
+if ! command -v prettier 2>&1 >/dev/null
+then
+        npm install -g prettier
+else
+        npm update -g prettier
+fi
