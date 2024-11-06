73f785295bddbefe10e7f22717ae1228f22952d5
BaldFabi <thebaldfabi@gmail.com>
2024-11-06 21:56:55 +0100

    fix: set missing ln for docker compose plugin

--- settings.sh
+++ settings.sh
@@ -14,3 +14,6 @@ then
 else
         npm update -g prettier
 fi
+
+mkdir -p $HOME/.docker/cli-plugins
+[ ! -f $HOME/.docker/cli-plugins/docker-compose ] && ln -s /opt/homebrew/bin/docker-compose $HOME/.docker/cli-plugins/docker-compose
