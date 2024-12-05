110051beb572b1d02ad8ef620ed9c70a8b0fbe06
BaldFabi <thebaldfabi@gmail.com>
2024-12-05 14:07:42 +0100

    feat(settings): add k9s theme

--- settings.sh
+++ settings.sh
@@ -17,3 +17,7 @@ fi
 
 mkdir -p $HOME/.docker/cli-plugins
 [ ! -f $HOME/.docker/cli-plugins/docker-compose ] && ln -s /opt/homebrew/bin/docker-compose $HOME/.docker/cli-plugins/docker-compose
+
+mkdir -p ~/Library/Application\ Support/k9s/skins
+yq -i '.k9s.ui.skin = "rose-pine"' ~/Library/Application\ Support/k9s/config.*
+curl -sSL https://raw.githubusercontent.com/derailed/k9s/refs/heads/master/skins/rose-pine.yaml -o ~/Library/Application\ Support/k9s/skins/rose-pine.yaml
