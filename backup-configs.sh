d7dd53c094fefb9459071264efd8dd9ffff1e6e7
BaldFabi <thebaldfabi@gmail.com>
2025-02-03 14:02:45 +0100

    feat: add config of alt-tab

--- backup-configs.sh
+++ backup-configs.sh
@@ -3,6 +3,7 @@
 cp ~/.config/karabiner/karabiner.json .
 cp ~/Library/Preferences/com.googlecode.iterm2.plist .
 cp ~/Library/Preferences/com.knollsoft.Rectangle.plist .
+cp ~/Library/Preferences/com.lwouis.alt-tab-macos.plist .
 cp ~/.zshrc .
 cp ~/.tmux.conf .
 cp ~/Library/Preferences/com.ospfranco.sol.plist .
--- restore-configs.sh
+++ restore-configs.sh
@@ -6,9 +6,10 @@ mkdir -p ~/.config/karabiner
 cp karabiner.json ~/.config/karabiner/karabiner.json
 cp com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist
 cp com.knollsoft.Rectangle.plist ~/Library/Preferences/com.knollsoft.Rectangle.plist
+cp com.ospfranco.sol.plist ~/Library/Preferences/com.ospfranco.sol.plist
+cp com.lwouis.alt-tab-macos.plist ~/Library/Preferences/com.lwouis.alt-tab-macos.plist
 cp .zshrc ~/.zshrc
 cp .tmux.conf ~/.tmux.conf
-cp com.ospfranco.sol.plist ~/Library/Preferences/com.ospfranco.sol.plist
 cp starship.toml ~/.config/starship.toml
 
 curl -fsSL https://git.io/antigen -o ~/antigen.zsh
