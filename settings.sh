63f2b272fa761e1ab8c25365777c5d36d0cad7bc
BaldFabi <thebaldfabi@gmail.com>
2025-02-03 09:57:17 +0100

    feat(settings): add some system settings

--- settings.sh
+++ settings.sh
@@ -3,10 +3,12 @@
 defaults write com.apple.finder AppleShowAllExtensions -bool true
 defaults write com.apple.dock autohide -bool true
 defaults write com.apple.dock largesize -int 90
+defaults write com.apple.dock tilesize -int 60
 defaults write com.apple.dock magnification -bool true
+defaults write com.apple.dock autohide-delay -int 0
+defaults write com.apple.dock show-recents -bool false
 defaults write com.apple.menuextra.battery ShowPercent -bool true
 defaults write com.apple.driver.AppleMultitouchTrackpad Clicking -bool true
-defaults write com.apple.dock autohide-delay -int 0
 
 if ! command -v prettier 2>&1 >/dev/null
 then
