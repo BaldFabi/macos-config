cbe4fa30db4736517dd25a4322561fcb6125244e
BaldFabi <thebaldfabi@gmail.com>
2025-07-01 13:12:18 +0200

    feat: set default git editor

--- settings.sh
+++ settings.sh
@@ -31,3 +31,5 @@ mkdir -p $HOME/.docker/cli-plugins
 mkdir -p ~/Library/Application\ Support/k9s/skins
 yq -i '.k9s.ui.skin = "rose-pine"' ~/Library/Application\ Support/k9s/config.*
 curl -sSL https://raw.githubusercontent.com/derailed/k9s/refs/heads/master/skins/rose-pine.yaml -o ~/Library/Application\ Support/k9s/skins/rose-pine.yaml
+
+git config --global core.editor "vim"
